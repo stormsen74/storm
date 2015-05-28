2796e8f51a3ddeb11b31cd7b85c81f87b645b3a7
Michael Sturm <michael.sturm@gghlowe.de>
2015-05-28 15:44:19 +0200

    ...

--- lib/perlin/perlin.d.ts
+++ lib/perlin/perlin.d.ts
@@ -4,9 +4,9 @@
 declare module noise {
     export function simplex2(x:number, y:number):number;
 
-    export function   simplex3(x:number, y:number, z:number):number;
-    export function   perlin2(x:number, y:number):number;
-     export function  perlin3(x:number, y:number, z:number):number;
-    export function   seed(value:number);
+    export function simplex3(x:number, y:number, z:number):number;
+    export function perlin2(x:number, y:number):number;
+    export function perlin3(x:number, y:number, z:number):number;
+    export function seed(value:number);
 }
 
