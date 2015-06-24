3eaf11aa5a0c49acaa43b164aa04a73dd76cba03
stormsen26 <micha@edit23.de>
2015-06-24 22:46:23 +0200

    added renderUpdate ...

--- scripts/Filters.ts
+++ scripts/Filters.ts
@@ -25,15 +25,11 @@ class Filters {
         that = this;
 
         this.pRenderer = PIXI.autoDetectRenderer(this.SIZE.x, this.SIZE.y);
-
         this.pStage = new PIXI.Container();
-        this.pStage.interactive = true;
-
         this.container = new PIXI.Container();
-
-
         this.pManager = new PIXI.interaction.InteractionManager(this.pRenderer);
 
+        this.pStage.interactive = true;
 
         //this.pRenderer.view.style.width = window.innerWidth + "px";
         //this.pRenderer.view.style.height = window.innerHeight + "px";
@@ -44,37 +40,31 @@ class Filters {
         this.pRenderer.view.style.display = "block";
         this.pRenderer.view.style.overflow = "hidden";
 
-
         // add render view to DOM
         document.body.appendChild(this.pRenderer.view);
 
 
         this.sprite = PIXI.Sprite.fromImage('assets/mountain_1280x720.jpg');
         this.container.addChild(this.sprite);
-
-
         this.pStage.addChild(this.container);
-
-        this.pRenderer.render(this.pStage);
-
 
         this.addFilter();
 
         this.addGUI();
 
+        TweenLite.delayedCall(1, function () {
+            that.renderStage()
+        });
 
-        this.animate();
+        //this.animate();
 
     }
 
     private addFilter() {
         this.filter = new PIXI.filters.TiltShiftFilter();
-        this.filter.blur = 20;
-        //this.filter.gradientBlur = 150;
+        //this.filter.blur = 20;
+        this.filter.gradientBlur = 300;
         this.container.filters = [this.filter];
-
-        //this.pRenderer.render(this.pStage);
-
 
     }
 
@@ -82,21 +72,31 @@ class Filters {
     // http://www.goodboydigital.com/pixijs/examples/15/indexAll.html
 
     private addGUI() {
-        var gui = new dat.GUI({});
+        var gui = new dat.GUI({width: 300, closed: false});
 
-        var filterFolder = gui.addFolder('TiltShiftFilter');
-        filterFolder.add(this.filter, 'blur', 0, 100);
-        //filterFolder.add(this.gradientBlur, 'blur', 0, 300);
+
+        var fFolder = gui.addFolder('TiltShiftFilter');
+        fFolder.open();
+        //var fController = fFolder.add(this.filter, 'blur', 0, 100);
+        var fController = fFolder.add(this.filter, 'gradientBlur', 0, 600);
+
+
+        fController.onChange(function (value) {
+            // Fires on every change, drag, keypress, etc.
+
+            that.renderStage();
+        });
+    }
+
+    private renderStage() {
+        this.pRenderer.render(this.pStage);
     }
 
     private animate() {
 
-
-
-        this.pRenderer.render(this.pStage);
+        this.renderStage();
 
         //this.stats.update();
-
 
         requestAnimationFrame(() => this.animate());
 
